2d94859ee534103de3b293199abd716ed1fb82fd
Neco_Stack <necoabay@googlemail.com>
2024-09-29 21:15:38 +0200

    1_7

---
+++ src/String-Methods-TS-Level-1_7/basic.ts
@@ -0,0 +1,8 @@
+const samsStatus= "Sam is good at codingschool"
+console.log(samsStatus.replace("good", "bad"));
+console.log(samsStatus.replace("bad", "good"));
+console.log(samsStatus.replace("codingschool", "tennis"));
+
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href='./src/String-Methods-TS-Level-1_7/basic.html'>Intro TS</a>
+
+  </div>
+`
